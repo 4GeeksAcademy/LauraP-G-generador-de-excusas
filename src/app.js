e130322d848f0111026964a12c0d5deac9a26333
Laura <151856378+LauraP-G@users.noreply.github.com>
2024-01-15 16:48:01 +0000

    1-JS proyecto Generador excusas finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
+import "./index.html";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+let action = ["ate ", "peed ", "crushed ", "broke "];
+let what = ["my homework ", "the keys ", "the car "];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.getElementById("excuse").innerHTML = crearExcusa();
+
+function crearExcusa() {
+  for (let n = 3; n < who.length; n++) {
+    return (
+      who[Math.floor(Math.random() * who.length)] +
+      action[Math.floor(Math.random() * action.length)] +
+      what[Math.floor(Math.random() * what.length)] +
+      when[Math.floor(Math.random() * when.length)]
+    );
+  }
+}
